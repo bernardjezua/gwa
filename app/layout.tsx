527e9c57ab675ae32fbfe93bd211abefacb45530
bernardjezua <bernardjezuaml@gmail.com>
2025-06-06 20:49:41 +0800

    feat: added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={`min-h-screen bg-background font-sans antialiased p-0 m-0 w-full ${inter.className}`}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
